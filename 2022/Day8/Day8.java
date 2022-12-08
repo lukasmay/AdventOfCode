22761c00b9825ff61c78af3fc3905406f7527bbc
lukasmay <76849604+lukasmay@users.noreply.github.com>
2022-12-08 02:04:54 +0000

    File name changes and prep for day8

---
+++ 2022/Day8/Day8.java
@@ -0,0 +1,20 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.lang.CharSequence;
+
+public class Day8 {
+    try {
+        Scanner scanner = new Scanner(new File("2022/Input/day8.txt"));
+        while (scanner.hasNextLine()) {
+        
+        
+        
+        }
+
+        scanner.close();
+    } catch (FileNotFoundException e) {
+        e.printStackTrace();
+    }
+
+}
