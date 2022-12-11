38ec00add1daca01883d7d25fa303ec971f5e6bd
lukasmay <76849604+lukasmay@users.noreply.github.com>
2022-12-11 19:52:14 +0000

    Started the outline code and basic beginings

--- 2022/Day11/Day11.java
+++ 2022/Day11/Day11.java
@@ -1,9 +1,10 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 /*
- * The way to do this is to create a class for the money to store all their data
+ * The way to do this is to create a class for the monkey to store all their data
  * Then loop through all of the rounds
  * When they are done add find the monkey with the most activity.
  *      This would be stored in every monkey
@@ -22,11 +23,33 @@ public class Day11 {
 
     public static void part1(){
         String line = "";
+        String[] data = new String[6];
+        int counter = 0;
+        ArrayList<Monkey> monkeys = new ArrayList<>();
+
+        String name = "";
+        Integer[] items = {0, 0};
+        String operation = "";
+        String test = "";
+        String ifTrue = "";
+        String ifFalse = "";
+
         try {
             Scanner scanner = new Scanner(new File("2022/input/day8.txt"));
             while (scanner.hasNextLine()) {
-                //Adding Rows to forest
                 line = scanner.nextLine();
+                data[counter] = line;
+                if (counter == 6){
+
+                    //Input the name for the ifTrue false stuff then a search could be done to add them the item thrown
+
+                    //I need to parse the lines to get the data that I want
+                    //Use the variables above to store the data
+                    //This is where the monkey will be created
+                    monkeys.add(new Monkey());
+                    counter = 0;
+                }
+                counter++;
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
---
+++ 2022/Day11/Monkey.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+
+public class Monkey {
+    private String name;
+    private ArrayList<Integer> items;
+    private String operation;
+    private String test;
+    private String ifTrue;
+    private String ifFalse;
+
+    public Monkey(String name, ArrayList<Integer> items, String operation, String test, String ifTrue, String ifFalse) {
+        this.name = name;
+        this.items = items;
+        this.operation = operation;
+        this.test = test;
+        this.ifTrue = ifTrue;
+        this.ifFalse = ifFalse;
+    }
+    public Monkey(){
+
+    }
+    
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Integer> getItems() {
+        return this.items;
+    }
+
+    public void setItems(ArrayList<Integer> items) {
+        this.items = items;
+    }
+
+    public String getOperation() {
+        return this.operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
+    public String getTest() {
+        return this.test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+
+    public Monkey getIfTrue() {
+        return this.ifTrue;
+    }
+
+    public void setIfTrue(Monkey ifTrue) {
+        this.ifTrue = ifTrue;
+    }
+
+    public Monkey getIfFalse() {
+        return this.ifFalse;
+    }
+
+    public void setIfFalse(Monkey ifFalse) {
+        this.ifFalse = ifFalse;
+    }
+}
