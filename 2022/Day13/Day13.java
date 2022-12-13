9f1417c514f4fb9fbc90e36110129840d32400fc
lukasmay <76849604+lukasmay@users.noreply.github.com>
2022-12-13 05:38:36 +0000

    Working

--- 2022/Day13/Day13.java
+++ 2022/Day13/Day13.java
@@ -10,16 +10,40 @@ public class Day13{
     		//part2();
     	}
 
+        public static String getSection(String packet, int section){
+            String beginSection = "";
+            String answer = "";
+            for (int i = 0; i < section; i++){
+            beginSection = packet.substring(0, packet.indexOf("]"));
+            answer = beginSection.substring(beginSection.lastIndexOf("[")+1);
+            packet = beginSection.substring(beginSection.indexOf("]")+1);
+            }
+            return answer;
+        }
+
         public static void part1(){
             String line = "";
+            String packet1 = "";
+            String packet2 = "";
+            int lineCounter = 0;
+            int solution = 0;
             try {
                 Scanner scanner = new Scanner(new File("2022/input/day13.txt"));
                 while (scanner.hasNextLine()) {
                     line = scanner.nextLine();
-                    if (line.equals("\n\n")){
-                        System.out.println("he");
-                    }
+                    if (line.trim().equals("")){
+                        lineCounter = -1;
 
+                        System.out.println(packet1);
+                        
+                    } else {
+                        if (lineCounter == 0){
+                            packet1 = line;
+                        } else {
+                            packet2 = line;
+                        }
+                    }
+                    lineCounter++;
                 }
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
