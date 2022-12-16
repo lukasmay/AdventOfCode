d166bfa629558d02f61e86097e8100f678f08a06
lukasmay <76849604+lukasmay@users.noreply.github.com>
2022-12-16 15:40:44 +0000

    Save

--- 2022/Day16/Day16.java
+++ 2022/Day16/Day16.java
@@ -6,9 +6,10 @@ import java.util.ArrayList;
 import java.io.*;
 
 public class Day16{
-
+    public static ArrayList<String[]> possiblePermutation = new ArrayList<>();
+    public static int counter = 0;
     public static void main(String[] args) {
-
+            
     		part1();
     		//part2();
     	}
@@ -21,10 +22,6 @@ public class Day16{
                 Scanner scanner = new Scanner(new File("2022/input/day16.txt"));
                 while (scanner.hasNextLine()) {
                     line = scanner.nextLine();
-                    System.out.print(line.substring(23, line.indexOf(";"))+" ");
-                    System.out.print(line.substring(6, 8)+" ");
-                    System.out.println(line.substring(line.indexOf(",")-2).strip().split(","));
-                    
                     valves.add(new Valve(Integer.valueOf(line.substring(23, line.indexOf(";"))), line.substring(6, 8), line.substring(line.indexOf(",")-2).strip().split(",")));
 
                 }
@@ -44,7 +41,7 @@ public class Day16{
              */
             String[] s = new String[valves.size()];
             for (int i = 0; i < valves.size(); i++){
-                s[i] = valves.get(i).getName();                
+                s[i] = valves.get(i).getName();
             }
             printAllRecursive(s.length, s, ' ');
             System.out.println("Done");
@@ -63,16 +60,19 @@ public class Day16{
               if(n == 1) {
                   printArray(elements, delimiter);
               } else {
-                  for(int i = 0; i < n-1; i++) {
-                      printAllRecursive(n - 1, elements, delimiter);
-                      if(n % 2 == 0) {
-                          swap(elements, i, n-1);
-                      } else {
-                          swap(elements, 0, n-1);
-                      }
-                  }
-                  printAllRecursive(n - 1, elements, delimiter);
-              }
+                    for(int i = 0; i < n-1; i++) {
+                        printAllRecursive(n - 1, elements, delimiter);
+                        if(n % 2 == 0) {
+                            swap(elements, i, n-1);
+                        } else {
+                            swap(elements, 0, n-1);
+                        }
+                    }
+                    counter++;
+                  
+                    printAllRecursive(n - 1, elements, delimiter);
+                  
+            }
           }
 
           private static <T> void swap(T[] elements, int a, int b) {
@@ -85,6 +85,9 @@ public class Day16{
             String delimiterSpace = delimiter + " ";
             for(int i = 0; i < elements.length; i++) {
                 //System.out.print(elements[i] + delimiterSpace);
+                //Change this to check if it is possible
+                    //If possible change add to array
+                
             }
             //System.out.print('\n');
         }
